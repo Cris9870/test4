c6a1f941a6361eb2a25298e521c51b91182e1b0b
Cris9870 <cmurillo9870@gmail.com>
2026-06-20 19:46:37 -0500

    Revert "test(deploy): marcador temporal para verificar webhook + deploy actions en lab139"
    This reverts commit 7ddad2ac78e5a2c9063553f07c1a33594bac075b.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- b1-deploy-check:7f3a9c -->
     <title>@yield('title', config('app.name'))</title>
 
     {{-- Tailwind compilado por Vite en el mini-server (public/build) --}}
